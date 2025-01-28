e144d1267cf947a55b17f9c6a6f7e9ce9b13cd6e
jMnnnnn <pj_teme@hotmail.com>
2025-01-28 22:55:41 +0700

    feat: job

---
+++ internal/job/repository.go
@@ -0,0 +1,61 @@
+package job
+
+import (
+	"context"
+
+	"github.com/kappusuton-yon-tebaru/backend/internal/models"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Repository struct {
+	job *mongo.Collection
+}
+
+func NewRepository(client *mongo.Client) *Repository {
+	return &Repository{
+		job: client.Database("Capstone").Collection("jobs"),
+	}
+}
+
+func (r* Repository) GetAllJobs(ctx context.Context) ([]models.Job, error) {
+	cur, err := r.job.Find(ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+
+	defer cur.Close(ctx)
+
+	jobs := make([]models.Job, 0)
+
+	for cur.Next(ctx) {
+		var dto JobDTO
+
+		err = cur.Decode(&dto)
+		if err != nil {
+			return nil, err
+		}
+
+		jobs = append(jobs, DTOToJob(dto))
+	}
+
+	return jobs, nil
+}
+
+func (r *Repository) CreateJob(ctx context.Context, dto CreateJobDTO) error {
+	_, err := r.job.InsertOne(ctx, dto)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r * Repository) DeleteJob(ctx context.Context, filter any) (int64, error) {
+	result, err := r.job.DeleteOne(ctx, filter)
+	if err != nil {
+		return 0, err
+	}
+
+	return result.DeletedCount, nil
+}
---
+++ internal/job/service.go
@@ -0,0 +1,64 @@
+package job
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/kappusuton-yon-tebaru/backend/internal/models"
+	"github.com/kappusuton-yon-tebaru/backend/internal/werror"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type Service struct {
+	repo *Repository
+}
+
+func NewService(repo *Repository) *Service {
+	return &Service{
+		repo,
+	}
+}
+
+func (s *Service) GetAllJobs(ctx context.Context) ([]models.Job, error) {
+	jobs, err := s.repo.GetAllJobs(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return jobs, nil
+}
+
+// func (s *Service) CreateJob(ctx context.Context, dto CreateJobDTO) error {
+// 	err := s.repo.CreateJob(ctx, dto)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	return nil
+// }
+
+func (s *Service) DeleteJob(ctx context.Context, id string) *werror.WError {
+	objId, err := bson.ObjectIDFromHex(id)
+	if err != nil {
+		return werror.NewFromError(err).
+			SetCode(http.StatusBadRequest).
+			SetMessage("invalid job id")
+	}
+
+	filter := map[string]any{
+		"_id": objId,
+	}
+
+	count, err := s.repo.DeleteJob(ctx, filter)
+	if err != nil {
+		return werror.NewFromError(err)
+	}
+
+	if count == 0 {
+		return werror.New().
+			SetCode(http.StatusNotFound).
+			SetMessage("not found")
+	}
+
+	return nil
+}
