7125755c2853f5137be1e007c9cc9ec2cec46d33
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-02-01 14:14:58 +0700

    feat: clean up func

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/kappusuton-yon-tebaru/backend/cmd/backend/backend"
 	"github.com/kappusuton-yon-tebaru/backend/cmd/backend/internal/router"
@@ -14,10 +18,31 @@ func main() {
 	}
 
 	r := router.New()
-
 	r.RegisterRoutes(app)
 
-	if err := r.Run(fmt.Sprintf("0.0.0.0:%v", app.Config.Backend.Port)); err != nil {
-		panic(err)
-	}
+	go func() {
+		if err := r.Run(fmt.Sprintf("0.0.0.0:%v", app.Config.Backend.Port)); err != nil {
+			panic(err)
+		}
+	}()
+
+	<-waitForCleanup(app)
+}
+
+func waitForCleanup(app *backend.App) <-chan bool {
+	done := make(chan bool, 1)
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sig
+
+		if err := app.MongoClient.Disconnect(context.Background()); err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+
+		done <- true
+	}()
+
+	return done
 }
