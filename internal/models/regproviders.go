a3008730fc27d4f84c7d67315493bd05154dfe2f
jMnnnnn <pj_teme@hotmail.com>
2025-02-15 18:41:21 +0700

    feat: add uri to registry provider

--- internal/models/regproviders.go
+++ internal/models/regproviders.go
@@ -3,6 +3,7 @@ package models
 type RegistryProviders struct {
 	Id		  		string 	`json:"id"`
 	Name	  		string 	`json:"name"`
+	URI		  		string 	`json:"uri"`
 	ProviderType	string	`json:"provider_type"` // enum
 	JsonCredential	string	`json:"json_credential"`
 	OrganizationId	string	`json:"organization_id"`
--- internal/regproviders/dto.go
+++ internal/regproviders/dto.go
@@ -6,15 +6,17 @@ import (
 )
 
 type RegistryProvidersDTO struct {
-	Id             bson.ObjectID 	`bson:"_id"`
-	Name           string 			`bson:"name"`
-	ProviderType   string 			`bson:"provider_type"` // enum
-	JsonCredential string 			`bson:"json_credential"`
-	OrganizationId bson.ObjectID 	`bson:"organization_id"`
+	Id             	bson.ObjectID 	`bson:"_id"`
+	Name           	string 			`bson:"name"`
+	URI				string 			`bson:"uri"`
+	ProviderType   	string 			`bson:"provider_type"` // enum
+	JsonCredential 	string 			`bson:"json_credential"`
+	OrganizationId 	bson.ObjectID 	`bson:"organization_id"`
 }
 
 type CreateRegistryProvidersDTO struct {
 	Name           string 			`bson:"name"`
+	URI            string 			`bson:"uri"`
 	ProviderType   string 			`bson:"provider_type"` // enum
 	JsonCredential string 			`bson:"json_credential"`
 	OrganizationId bson.ObjectID 	`bson:"organization_id"`
@@ -24,6 +26,7 @@ func DTOToRegistryProviders(regProviders RegistryProvidersDTO) models.RegistryPr
 	return models.RegistryProviders{
 		Id:             regProviders.Id.Hex(),
 		Name:           regProviders.Name,
+		URI:            regProviders.URI,
 		ProviderType:   regProviders.ProviderType,
 		JsonCredential: regProviders.JsonCredential,
 		OrganizationId: regProviders.OrganizationId.Hex(),
