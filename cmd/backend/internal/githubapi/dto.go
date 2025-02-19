19bb6498610960d8a7800b2fde4b3ef424175c59
WinSukon <jakrinsukon@gmail.com>
2025-02-19 10:54:19 +0700

    fix: dto

--- cmd/backend/internal/githubapi/dto.go
+++ cmd/backend/internal/githubapi/dto.go
@@ -5,13 +5,13 @@ type GetRepoContentsRequest struct {
 	Repo   string `validate:"required"`
 	Path   string `validate:"omitempty"`
 	Branch string `validate:"omitempty"`
-	Token  string `validate:"required,jwt"`
+	Token  string `validate:"required"`
 }
 
 type GetRepoBranchesRequest struct {
 	Owner string `validate:"required"`
 	Repo  string `validate:"required"`
-	Token string `validate:"required,jwt"`
+	Token string `validate:"required"`
 }
 
 type GetCommitMetadataRequest struct {
@@ -19,7 +19,7 @@ type GetCommitMetadataRequest struct {
 	Repo   string `validate:"required"`
 	Path   string `validate:"required"` // Path is required to fetch commit metadata
 	Branch string `validate:"required"` // Branch is required to get correct commit history
-	Token  string `validate:"required,jwt"`
+	Token  string `validate:"required"`
 }
 
 type FetchFileContentRequest struct {
@@ -27,7 +27,7 @@ type FetchFileContentRequest struct {
 	Repo     string `validate:"required"`
 	FilePath string `validate:"required"` // File path is required to fetch content
 	Branch   string `validate:"required"` // Branch is required for versioning
-	Token    string `validate:"required,jwt"`
+	Token    string `validate:"required"`
 }
 
 type CreateBranchRequest struct {
@@ -35,7 +35,7 @@ type CreateBranchRequest struct {
 	Repo           string `validate:"required"`
 	BranchName     string `validate:"required"` // New branch name is required
 	SelectedBranch string `validate:"required"` // Base branch to create from is required
-	Token          string `validate:"required,jwt"`
+	Token          string `validate:"required"`
 }
 
 type UpdateFileContentRequest struct {
@@ -46,10 +46,10 @@ type UpdateFileContentRequest struct {
 	Base64Content string `json:"base64Content" validate:"required"` // Base64 encoded file content
 	Sha           string `json:"sha" validate:"required"`           // SHA of the existing file
 	Branch        string `json:"branch" validate:"required"`        // Target branch
-	Token         string `validate:"required,jwt"`
+	Token         string `validate:"required"`
 }
 
 type GetServicesRequest struct {
 	ProjectID string `validate:"required"` // project ID is required
-	Token     string `validate:"required,jwt"`
+	Token     string `validate:"required"`
 }
