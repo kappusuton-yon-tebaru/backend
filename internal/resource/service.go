3f5383229f8d5085d8d917f863ac12da68108785
WinSukon <jakrinsukon@gmail.com>
2025-02-19 11:57:45 +0700

    fix: create resource return resourceId

--- internal/resource/service.go
+++ internal/resource/service.go
@@ -62,7 +62,7 @@ func (s *Service) GetChildrenResourcesByParentID(ctx context.Context, parentID s
 	}
 
 	filter := map[string]any{
-		"parent_resource_id": objId,
+		"parent_resourceId": objId,
 	}
 	// get children rela
 	childrenResourceRelas, err := s.resourceRelaRepo.GetChildrenResourceRelationshipByParentID(ctx, filter)
@@ -93,13 +93,13 @@ func (s *Service) GetChildrenResourcesByParentID(ctx context.Context, parentID s
 }
 
 func (s *Service) CreateResource(ctx context.Context, dto CreateResourceDTO, id string) (string, error) {
-	resource_id, err := s.repo.CreateResource(ctx, dto)
+	resourceId, err := s.repo.CreateResource(ctx, dto)
 	if err != nil {
 		return "", err
 	}
 	// Is an org no need to create rela
 	if id == "" {
-		return resource_id, nil
+		return resourceId, nil
 	}
 
 	parentID, err := bson.ObjectIDFromHex(id)
@@ -108,7 +108,7 @@ func (s *Service) CreateResource(ctx context.Context, dto CreateResourceDTO, id 
 		return "", err
 	}
 
-	childID, err := bson.ObjectIDFromHex(resource_id)
+	childID, err := bson.ObjectIDFromHex(resourceId)
 	if err != nil {
 		fmt.Println("Invalid child ID:", err)
 		return "", err
@@ -120,12 +120,12 @@ func (s *Service) CreateResource(ctx context.Context, dto CreateResourceDTO, id 
 		ChildResourceId:  childID,
 	}
 
-	resource_rela_id, err := s.resourceRelaRepo.CreateResourceRelationship(ctx, relationship)
+	_, err = s.resourceRelaRepo.CreateResourceRelationship(ctx, relationship)
 	if err != nil {
 		return "", err
 	}
 
-	return resource_rela_id, nil
+	return resourceId, nil
 }
 
 func (s *Service) DeleteResource(ctx context.Context, id string) *werror.WError {
