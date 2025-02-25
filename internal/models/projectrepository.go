25d6349a70443728b5e5696265040051d7e0c6a8
jMnnnnn <pj_teme@hotmail.com>
2025-02-25 22:24:08 +0700

    feat: timestamp in projectrepo and regprovider

--- internal/models/projectrepository.go
+++ internal/models/projectrepository.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type ProjectRepository struct {
 	Id               string            `json:"id"`
 	ProjectId        string            `json:"project_id"`
 	GitRepoUrl       string            `json:"git_repo_url"`
 	RegistryProvider RegistryProviders `json:"registry_provider"`
+	CreatedAt        time.Time         `json:"created_at"`
+	UpdatedAt        time.Time         `json:"updated_at"`
 }
