34a426f1c6e8a76030949e4e491ebac83dd6b594
jMnnnnn <pj_teme@hotmail.com>
2025-02-25 22:24:08 +0700

    feat: timestamp in projectrepo and regprovider

--- internal/models/regproviders.go
+++ internal/models/regproviders.go
@@ -1,10 +1,14 @@
 package models
 
+import "time"
+
 type RegistryProviders struct {
-	Id             string `json:"id"`
-	Name           string `json:"name"`
-	ProviderType   string `json:"provider_type"` // enum
-	Uri            string `json:"uri"`
-	JsonCredential string `json:"json_credential"`
-	OrganizationId string `json:"organization_id"`
+	Id             string    `json:"id"`
+	Name           string    `json:"name"`
+	ProviderType   string    `json:"provider_type"` // enum
+	Uri            string    `json:"uri"`
+	JsonCredential string    `json:"json_credential"`
+	OrganizationId string    `json:"organization_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
--- internal/regproviders/dto.go
+++ internal/regproviders/dto.go
@@ -1,17 +1,21 @@
 package regproviders
 
 import (
+	"time"
+
 	"github.com/kappusuton-yon-tebaru/backend/internal/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type RegistryProvidersDTO struct {
-	Id             bson.ObjectID `bson:"_id"`
-	Name           string        `bson:"name"`
-	ProviderType   string        `bson:"provider_type"` // enum
-	JsonCredential string        `bson:"json_credential"`
-	Uri            string        `bson:"uri"`
-	OrganizationId bson.ObjectID `bson:"organization_id"`
+	Id             	bson.ObjectID 	`bson:"_id"`
+	Name           	string        	`bson:"name"`
+	ProviderType   	string        	`bson:"provider_type"` // enum
+	JsonCredential 	string        	`bson:"json_credential"`
+	Uri            	string        	`bson:"uri"`
+	OrganizationId 	bson.ObjectID 	`bson:"organization_id"`
+	CreatedAt	  	time.Time      	`bson:"created_at"`
+	UpdatedAt	  	time.Time      	`bson:"updated_at"`
 }
 
 type CreateRegistryProvidersDTO struct {
@@ -20,6 +24,8 @@ type CreateRegistryProvidersDTO struct {
 	Uri            string        `bson:"uri" json:"uri"`
 	JsonCredential string        `bson:"json_credential" json:"json_credential"`
 	OrganizationId bson.ObjectID `bson:"organization_id" json:"organization_id"`
+	CreatedAt	  	time.Time      	`bson:"created_at"`
+	UpdatedAt	  	time.Time      	`bson:"updated_at"`
 }
 
 func DTOToRegistryProviders(regProviders RegistryProvidersDTO) models.RegistryProviders {
@@ -30,5 +36,7 @@ func DTOToRegistryProviders(regProviders RegistryProvidersDTO) models.RegistryPr
 		JsonCredential: regProviders.JsonCredential,
 		Uri:            regProviders.Uri,
 		OrganizationId: regProviders.OrganizationId.Hex(),
+		CreatedAt:      regProviders.CreatedAt,
+		UpdatedAt:      regProviders.UpdatedAt,
 	}
 }
