2dbf429ccb864f0b6f0d370f93f4c992b570742f
jMnnnnn <pj_teme@hotmail.com>
2025-02-26 13:27:43 +0700

    feat: pagination ecr

--- cmd/backend/internal/ecr/service.go
+++ cmd/backend/internal/ecr/service.go
@@ -1,6 +1,10 @@
 package ecr
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/kappusuton-yon-tebaru/backend/internal/models"
+)
 
 type Service struct {
 	repo *ECRRepository
@@ -12,20 +16,23 @@ func NewService(repo *ECRRepository) *Service {
 	}
 }
 
-func (s *Service) GetECRImages(repoURI string, serviceName string) ([]ECRImageResponse, error) {
+func (s *Service) GetECRImages(repoURI string, serviceName string, pagination models.Pagination) (models.Paginated[ECRImageResponse], error) {
 	images, err := s.repo.GetImages(repoURI)
 	if err != nil {
-		return nil, err
+		return models.Paginated[ECRImageResponse]{}, err
 	}
 
 	var response []ECRImageResponse
 	for _, img := range images {
 		if strings.Contains(img, serviceName) {
-			response = append(response, ECRImageResponse{
-				ImageTag: img,
-			})
+			response = append(response, img)
 		}
 	}
 
-	return response, nil
+	return models.Paginated[ECRImageResponse]{
+		Page: pagination.Page,
+		Limit: pagination.Limit,
+		Total: len(response),
+		Data: response,
+	} , nil
 }
