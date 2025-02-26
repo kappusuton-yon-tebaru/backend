922e953046e5bd0f5d24d62e2f9a24589fdfe81e
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-02-26 16:56:16 +0700

    fix: nullable registry provider in project repo

--- internal/models/projectrepository.go
+++ internal/models/projectrepository.go
@@ -1,8 +1,8 @@
 package models
 
 type ProjectRepository struct {
-	Id               string            `json:"id"`
-	ProjectId        string            `json:"project_id"`
-	GitRepoUrl       string            `json:"git_repo_url"`
-	RegistryProvider RegistryProviders `json:"registry_provider"`
+	Id               string             `json:"id"`
+	ProjectId        string             `json:"project_id"`
+	GitRepoUrl       string             `json:"git_repo_url"`
+	RegistryProvider *RegistryProviders `json:"registry_provider"`
 }
--- internal/projectrepository/dto.go
+++ internal/projectrepository/dto.go
@@ -3,27 +3,33 @@ package projectrepository
 import (
 	"github.com/kappusuton-yon-tebaru/backend/internal/models"
 	"github.com/kappusuton-yon-tebaru/backend/internal/regproviders"
+	"github.com/kappusuton-yon-tebaru/backend/internal/utils"
 	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type ProjectRepositoryDTO struct {
-	Id               bson.ObjectID                     `bson:"_id"`
-	GitRepoUrl       string                            `bson:"git_repo_url"`
-	ProjectId        bson.ObjectID                     `bson:"project_id"`
-	RegistryProvider regproviders.RegistryProvidersDTO `bson:"registry_provider"`
+	Id               bson.ObjectID                      `bson:"_id"`
+	GitRepoUrl       string                             `bson:"git_repo_url"`
+	ProjectId        bson.ObjectID                      `bson:"project_id"`
+	RegistryProvider *regproviders.RegistryProvidersDTO `bson:"registry_provider"`
 }
 
 type CreateProjectRepositoryDTO struct {
-	GitRepoUrl string        `bson:"git_repo_url"`
+	GitRepoUrl string `bson:"git_repo_url"`
 	// ProjectId  bson.ObjectID `bson:"project_id"`
 }
 
 func DTOToProjectRepository(projrepo ProjectRepositoryDTO) models.ProjectRepository {
+	regProvider := (*models.RegistryProviders)(nil)
+	if projrepo.RegistryProvider != nil {
+		regProvider = utils.Pointer(regproviders.DTOToRegistryProviders(*projrepo.RegistryProvider))
+	}
+
 	return models.ProjectRepository{
 		Id:               projrepo.Id.Hex(),
 		GitRepoUrl:       projrepo.GitRepoUrl,
 		ProjectId:        projrepo.ProjectId.Hex(),
-		RegistryProvider: regproviders.DTOToRegistryProviders(projrepo.RegistryProvider),
+		RegistryProvider: regProvider,
 	}
 }
 
