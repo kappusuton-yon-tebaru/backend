10a3b4e0c35023f9fe22c8c8295af3486785a2d6
jMnnnnn <pj_teme@hotmail.com>
2025-02-26 14:02:01 +0700

    fix: ecr timestamps

--- internal/models/regproviders.go
+++ internal/models/regproviders.go
@@ -7,15 +7,14 @@ import (
 )
 
 type RegistryProviders struct {
-	Id             string                    `json:"id"`
-	Name           string                    `json:"name"`
-	ProviderType   enum.RegistryProviderType `json:"provider_type"`
-	Uri            string                    `json:"uri"`
-	Credential     interface{}               `json:"credential"`
-	OrganizationId string                    `json:"organization_id"`
-	CreatedAt	  	time.Time      	`json:"created_at"`
-	UpdatedAt	  	time.Time      	`json:"updated_at"`
-
+	Id             string                       `json:"id"`
+	Name           string                       `json:"name"`
+	ProviderType   enum.RegistryProviderType    `json:"provider_type"`
+	Uri            string                       `json:"uri"`
+	Credential     interface{}                  `json:"credential"`
+	OrganizationId string                       `json:"organization_id"`
+	CreatedAt      time.Time 					`json:"created_at"`
+	UpdatedAt      time.Time 					`json:"updated_at"`
 }
 
 type ECRCredential struct {
