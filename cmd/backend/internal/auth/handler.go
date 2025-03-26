9bc48e1a815340dfb193fae05c9a47efe55f39db
Thanapat Ussawanarong <thanapat.usw@gmail.com>
2025-03-26 10:52:45 +0700

    chore: api docs

--- cmd/backend/internal/auth/handler.go
+++ cmd/backend/internal/auth/handler.go
@@ -2,10 +2,12 @@ package auth
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kappusuton-yon-tebaru/backend/internal/auth"
 	"github.com/kappusuton-yon-tebaru/backend/internal/config"
+	"github.com/kappusuton-yon-tebaru/backend/internal/httputils"
 	"github.com/kappusuton-yon-tebaru/backend/internal/user"
 	"github.com/kappusuton-yon-tebaru/backend/internal/validator"
 )
@@ -24,18 +26,29 @@ func NewHandler(config *config.Config, service *auth.Service, validator *validat
 	}
 }
 
+// Register
+//
+//	@Router			/auth/register [post]
+//	@Summary		Register
+//	@Description	Register
+//	@Tags			Auth
+//	@Produce		json
+//	@Param			request	body		RegisterRequest	true	"register request"
+//	@Success		200		{object}	AuthResponse
+//	@Failure		400		{object}	httputils.ErrResponse
+//	@Failure		500		{object}	httputils.ErrResponse
 func (h *Handler) Register(ctx *gin.Context) {
-	var req RegisterReq
+	var req RegisterRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, map[string]any{
-			"error": "invalid body",
+		ctx.JSON(http.StatusBadRequest, httputils.ErrResponse{
+			Message: "invalid body",
 		})
 		return
 	}
 
 	if err := h.validator.Struct(req); err != nil {
-		ctx.JSON(http.StatusBadRequest, map[string]any{
-			"errors": h.validator.Translate(err),
+		ctx.JSON(http.StatusBadRequest, httputils.ErrResponse{
+			Message: strings.Join(h.validator.Translate(err), ", "),
 		})
 		return
 	}
@@ -47,29 +60,38 @@ func (h *Handler) Register(ctx *gin.Context) {
 
 	werr := h.service.Register(ctx, credential)
 	if werr != nil {
-		ctx.JSON(werr.GetCodeOr(http.StatusInternalServerError), map[string]any{
-			"error": werr.GetMessageOr("internal server error"),
-		})
+		ctx.JSON(httputils.ErrorResponseFromWErr(werr))
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, map[string]any{
-		"message": "user created",
+	ctx.JSON(http.StatusCreated, AuthResponse{
+		Message: "user created",
 	})
 }
 
+// Login
+//
+//	@Router			/auth/login [post]
+//	@Summary		Login
+//	@Description	Login
+//	@Tags			Auth
+//	@Produce		json
+//	@Param			request	body		LoginRequest	true	"register request"
+//	@Success		200		{object}	AuthResponse
+//	@Failure		400		{object}	httputils.ErrResponse
+//	@Failure		500		{object}	httputils.ErrResponse
 func (h *Handler) Login(ctx *gin.Context) {
-	var req LoginReq
+	var req LoginRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, map[string]any{
-			"error": "invalid body",
+		ctx.JSON(http.StatusBadRequest, httputils.ErrResponse{
+			Message: "invalid body",
 		})
 		return
 	}
 
 	if err := h.validator.Struct(req); err != nil {
-		ctx.JSON(http.StatusBadRequest, map[string]any{
-			"errors": h.validator.Translate(err),
+		ctx.JSON(http.StatusBadRequest, httputils.ErrResponse{
+			Message: strings.Join(h.validator.Translate(err), ", "),
 		})
 		return
 	}
@@ -81,19 +103,28 @@ func (h *Handler) Login(ctx *gin.Context) {
 
 	token, werr := h.service.Login(ctx, credential)
 	if werr != nil {
-		ctx.JSON(werr.GetCodeOr(http.StatusInternalServerError), map[string]any{
-			"error": werr.GetMessageOr("internal server error"),
-		})
+		ctx.JSON(httputils.ErrorResponseFromWErr(werr))
 		return
 	}
 
 	ctx.SetCookie("session_token", token, h.config.SessionExpiresInSecond, "/", "localhost", false, true)
 
-	ctx.JSON(http.StatusOK, map[string]any{
-		"message": "logged in",
+	ctx.JSON(http.StatusOK, AuthResponse{
+		Message: "logged in",
 	})
 }
 
+// Logout
+//
+//	@Router			/auth/logout [post]
+//	@Summary		Logout
+//	@Description	Logout
+//	@Tags			Auth
+//	@Success		200
 func (h *Handler) Logout(ctx *gin.Context) {
 	ctx.SetCookie("session_token", "", -1, "/", "localhost", false, true)
+
+	ctx.JSON(http.StatusOK, AuthResponse{
+		Message: "logged out",
+	})
 }
